690a3429052ed16a6109fd96823541f21d026c0b
Satya Arjunan <satya.arjunan@gmail.com>
2012-06-18 15:02:38 +0900

    validated SNRP and overall working well version

--- plotHistogram.example.py
+++ plotHistogram.example.py
@@ -10,11 +10,11 @@ tickFontSize = 14
 legendFontSize = 14
 lineFontSize = 14
 
-fileNames = ['HisLog.csv']
+fileNames = ['Fig3.right.csv']
 legendTitles = ['State 2', 'State 1']
 speciesList = ['E']
 lines = ['-', '--', '-', '-']
-colors = ['r', 'b', 'g', 'r', 'c', 'k', '#009955', '#ff9933', '#ff00ff', '#11dd00']
+colors = ['r', 'b', '#000000', 'black', 'c', 'k', '#009955', '#ff9933', '#ff00ff', '#11dd00']
 data = genfromtxt(fileNames[0], delimiter=',')
 x = []
 currTime = float(data[0][0])
@@ -28,6 +28,8 @@ bins = len(x)
 timePoints = len(data)/bins
 y1 = [0]*bins
 y2 = [0]*bins
+y3 = [0.562]*bins
+y4 = [0.38]*bins
 for i in range(timePoints):
   for j in range(bins):
     y1[j] = y1[j] + data[i*bins+j][2]
@@ -37,12 +39,14 @@ for i in range(bins):
   y1[i] = y1[i]/timePoints/(700.0/bins)
   y2[i] = y2[i]/timePoints/(700.0/bins)
 
-plot(x, y1, ls=lines[0], color=colors[0], label=legendTitles[0], linewidth=2)
-plot(x, y2, ls=lines[0], color=colors[1], label=legendTitles[1], linewidth=2)
+plot(x, y3, ls=lines[0], color=colors[2], linewidth=1)
+plot(x, y4, ls=lines[0], color=colors[2], linewidth=1)
+plot(x, y1, ls=lines[0], color=colors[0], label=legendTitles[0], linewidth=1)
+plot(x, y2, ls=lines[0], color=colors[1], label=legendTitles[1], linewidth=1)
 
 ax = gca()
 ax.grid(color='b', linestyle='--')
-ax.yaxis.set_major_locator(MaxNLocator(14))
+#ax.yaxis.set_major_locator(MaxNLocator(14))
 leg = legend(loc=0, labelspacing=0.2, handletextpad=0.2, fancybox=True)
 for t in leg.get_texts():
   t.set_fontsize(legendFontSize)   
