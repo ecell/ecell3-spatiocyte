c7a0149cd7c8eb7981075371144e9769f082a16b
Satya Arjunan <satya.arjunan@gmail.com>
2013-06-03 13:10:23 +0900

    Make DiffusionProcess uninterruptible

--- DiffusionProcess.hpp
+++ DiffusionProcess.hpp
@@ -71,6 +71,7 @@ public:
   SIMPLE_SET_GET_METHOD(Real, P);
   SIMPLE_SET_GET_METHOD(Real, Propensity);
   SIMPLE_SET_GET_METHOD(Real, WalkProbability);
+  virtual void substrateValueChanged(double) {}
   virtual void initialize()
     {
       if(isInitialized)
